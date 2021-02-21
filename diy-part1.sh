12fbba9554b1f6e891b9025e9e26c14280f2a010
zyc18 <58498616+zyc18@users.noreply.github.com>
2021-02-21 22:14:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,4 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git OpenClash https://github.com/vernesong/OpenClash' feeds.conf.default
